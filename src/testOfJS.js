c8bc5af94b3b222eba751c09fc52e34d0f3a92d6
Léo <leo.poupet@gmail.com>
2022-02-07 16:00:07 +0100

    Update testOfJS.js
    Leadger test added

--- src/testOfJS.js
+++ src/testOfJS.js
@@ -140,3 +140,107 @@ race2();
 // Horse B
 // Horse A
 // coz microtask queue (originally called PromiseJobs ) that specifically manages Promises.
+
+
+// Ledger Test : 
+// const fromPairs = (elements) => {
+  
+//   const object = {};
+  
+//   elements.forEach(element => {
+    
+//     object[element[0]] = element[1];
+//   });
+             
+//   return object;
+// }
+
+// const res = fromPairs([
+//   ["a", 1],
+//   ["b", 2],
+// ]);
+// // => { 'a': 1, 'b': 2 }
+
+// console.log(res)
+
+// const flattenDepth = (array, depth) => {
+//   let flattened = [];
+//   array.forEach(value => {
+//     if (Array.isArray(value) && depth >= 1)
+//       flattened = flattened.concat(flattenDepth(value, depth - 1));
+//     else
+//       flattened.push(value);
+//   });
+  
+//   return flattened;
+// }
+
+// const array = [1, [2, [3, [4]]], 5]
+
+// const res = flattenDepth(array, 1);
+// console.log(res);
+// // => [1, 2, [3, [4]], 5]
+
+// const res2 = flattenDepth(array, 2);
+// // => [1, 2, 3, [4], 5]
+// console.log(res2);
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function square(n) {
+//   return n * n;
+// }
+
+// const flow = (callbacks) => (...params) => {
+//   return callbacks.reduce((acc, callback) => {
+//     return [callback(...acc)];
+//   }, params)[0];
+// }
+
+// const addSquare = flow([add, square]);
+// const res = addSquare(1, 2);
+// console.log(res)
+// // => 9
+
+// pic1: https://user-images.githubusercontent.com/211411/89523634-7a711200-d7e3-11ea-9e43-6d8ecd0250f2.png
+// pic2: https://user-images.githubusercontent.com/211411/89523628-77762180-d7e3-11ea-9e84-86e3e7a85e2e.png
+
+// prettier-ignore
+const accounts = [{"usdBalance":8.94,"balance":2547.2,"operations":[{"date":1596182061000,"value":1},{"date":1596182012000,"value":1},{"date":1593596465000,"value":500},{"date":1593596465000,"value":-500.226},{"date":1593421197000,"value":449},{"date":1593421197000,"value":-449.226},{"date":1593169928000,"value":2000},{"date":1593169928000,"value":-2000.226},{"date":1593168747000,"value":-1000.226},{"date":1593074058000,"value":-1.226},{"date":1593073216000,"value":-42.226},{"date":1593073152000,"value":-1.426},{"date":1591120198000,"value":-10.00000226},{"date":1590954610000,"value":-20.00000226},{"date":1590757275000,"value":-1.00000226},{"date":1589966865000,"value":-20.226},{"date":1589627057000,"value":-5.0000022600000005},{"date":1589618876000,"value":-1.00000374},{"date":1581418956000,"value":1},{"date":1581418956000,"value":-1.226},{"date":1581081895000,"value":3648.46340367},{"date":1581081895000,"value":-3649.09940367}],"currencyId":"dogecoin"},{"usdBalance":1.62,"balance":0.0044098,"operations":[{"date":1582640230000,"value":-0.000059072000000000006},{"date":1582640123000,"value":-0.000059072000000000006}],"currencyId":"ethereum"},{"usdBalance":0.16,"balance":44.32,"operations":[{"date":1596182061000,"value":-1.2},{"date":1596182012000,"value":-1.226},{"date":1594384212000,"value":20},{"date":1594384212000,"value":-20.226},{"date":1593168397000,"value":0.2},{"date":1593168397000,"value":-0.426},{"date":1593074058000,"value":1},{"date":1593073216000,"value":42},{"date":1593073184000,"value":2},{"date":1593073151000,"value":1.2},{"date":1555169370000,"value":1}],"currencyId":"dogecoin"},{"usdBalance":0,"balance":0.1,"operations":[{"date":1582640230000,"value":0.0001},{"date":1582640230000,"value":-0.0001},{"date":1582640123000,"value":0.0001},{"date":1582640123000,"value":-0.0001},{"date":1582640123000,"value":0.0001},{"date":1582640123000,"value":-0.0001},{"date":1582639559000,"value":0.0001},{"date":1582639559000,"value":-0.0001},{"date":1582639559000,"value":0.001},{"date":1582639559000,"value":-0.001},{"date":1582638742000,"value":0.001},{"date":1582638742000,"value":-0.001},{"date":1582638376000,"value":0.0001},{"date":1582638376000,"value":-0.0001},{"date":1582638229000,"value":0.0001},{"date":1582638229000,"value":-0.0001},{"date":1582638043000,"value":0.001},{"date":1582638043000,"value":-0.001},{"date":1582637710000,"value":0.00001},{"date":1582637710000,"value":-0.00001},{"date":1582637680000,"value":0.00001},{"date":1582637680000,"value":-0.00001},{"date":1582637486000,"value":0.001},{"date":1582637486000,"value":-0.001},{"date":1582636016000,"value":0.001},{"date":1582636016000,"value":-0.001},{"date":1565618009000,"value":0.1}],"currencyId":"amberToken"},{"usdBalance":0,"balance":0.1,"operations":[{"date":1555327189000,"value":0.1}],"currencyId":"dogecoin"},{"usdBalance":0,"balance":0.09,"operations":[{"date":1582639559000,"value":0.001},{"date":1582639559000,"value":-0.001},{"date":1582628047000,"value":-0.01},{"date":1561151893000,"value":0.1}],"currencyId":"daiToken"}]
+
+// type Account = {
+//   usdBalance: number,
+//   balance: number,
+//   operations: Operation[], // sorted by date, recent operations first
+//   currencyId: string
+// }
+
+// type Operation = {
+//   date: number, // timestamp
+//   value: number
+// }
+
+const assetsAllocation = (accounts) => {
+  const res = accounts.reduce((acc, account) => {
+    const { currencyId, usdBalance, balance } = account;
+    if (acc[currencyId]) {
+      return { ...acc, [currencyId]: { currencyId, usdAmount: acc[currencyId].usdAmount + usdBalance, amount:     acc[currencyId].amount + balance }}  ;
+    }
+    else {
+      return {...acc, [currencyId]: { currencyId, usdAmount: usdBalance, amount: balance } };
+    }
+  }, [])
+  
+  return Object.values(res);
+}
+
+console.log("assetsAllocation", assetsAllocation(accounts));
+
+// [
+//   { currencyId: "dogecoin", amount: 2591.62, usdAmount: 9.1 },
+//   { currencyId: "ethereum", amount: 0.0044098, usdAmount: 1.62 },
+//   { currencyId: "amberToken", amount: 0.1, usdAmount: 0 },
+//   { currencyId: "daiToken", amount: 0.09, usdAmount: 0 },
+// ];
